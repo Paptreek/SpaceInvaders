d4a3baebec25f5718a8ec0dab7120700d0d72e64
paptreek <paptreek@hotmail.com>
2026-02-24 00:29:31 -0600

    ufo effects
    - added some death effects to UFO
    - still working on particle effect

--- Assets/Scripts/UFO.cs
+++ Assets/Scripts/UFO.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class UFO : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private GameObject _explosionSound;
+    [SerializeField] private GameObject _explosionEffect;
+
+    private float _timer = 1.0f;
+    private bool _wasShot;
 
     private void Update()
     {
@@ -12,13 +17,40 @@ public class UFO : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (_wasShot)
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("PlayerBullet"))
         {
-            Destroy(gameObject);
+            Kill();
         }
+    }
+
+    private void Kill()
+    {
+        _wasShot = true;
+
+        Renderer[] components = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in components)
+        {
+            Destroy(renderer);
+        }
+
+        GetComponent<AudioSource>().Stop();
+        _explosionSound.GetComponent<AudioSource>().Play();
+        _explosionEffect.transform.position = transform.position;
+        _explosionEffect.GetComponent<ParticleSystem>().Play();
     }
 }
