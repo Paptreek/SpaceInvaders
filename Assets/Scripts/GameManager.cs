f118032410c55a966ecc93806fd5aa91f1a14964
paptreek <paptreek@hotmail.com>
2026-02-28 17:46:13 -0600

    game over logic
    - logic is in, currently printing to console when triggered

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,26 +3,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _playerObj;
     [SerializeField] private GameObject _scoreObj;
     [SerializeField] private TMP_Text _scoreText;
     [SerializeField] private TMP_Text _highScoreText;
+    [SerializeField] private TMP_Text _livesText;
+
+    private PlayerController _player;
 
     private int _currentScore;
-    private int _highScore;
+
+    private void Awake()
+    {
+        _player = _playerObj.GetComponent<PlayerController>();
+    }
 
     private void Update()
     {
         _currentScore = _scoreObj.GetComponent<Score>().CurrentScore;
         _scoreText.text = $"Score: {_currentScore:0000}";
 
-        _highScore = GetHighScore();
+        int _highScore = GetHighScore();
         _highScoreText.text = $"High Score: {_highScore:0000}";
+
+        int remainingLives = _player.LivesRemaining;
+        _livesText.text = $"Lives: {remainingLives}";
 
         if (_currentScore > _highScore)
         {
             SetHighScore();
         }
+
+        CheckForGameOver();
     }
 
     private void SetHighScore()
@@ -33,5 +45,13 @@ public class GameManager : MonoBehaviour
     private int GetHighScore()
     {
         return PlayerPrefs.GetInt("HighScore");
+    }
+
+    private void CheckForGameOver()
+    {
+        if (_player.LivesRemaining <= 0)
+        {
+            Debug.Log($"Game Over!");
+        }
     }
 }
